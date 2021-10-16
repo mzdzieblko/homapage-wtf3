a5e1d50de7c4561132dff27d67ecf25365e0dd25
Mateusz Zdzieblko <zdzieblko@icloud.com>
2021-10-16 23:53:37 +0200

    add div by JS

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ const firstName = 'Mateusz';
 const age = 32;
 
 console.log(`Nazywam się ${firstName} mam ${age} lat ;D `);
+
+const box = document.querySelector('.div--js');
+
+box.innerHTML = 'Treść podstawiona przez JS';
