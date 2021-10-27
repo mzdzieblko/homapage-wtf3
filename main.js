731e4884aab1d3379c1486f96023d91a9a14c8b7
Mateusz Zdzieblko <zdzieblko@icloud.com>
2021-10-27 09:48:41 +0200

    js treaning

--- main.js
+++ main.js
@@ -6,3 +6,24 @@ console.log(`Nazywam się ${firstName} mam ${age} lat ;D `);
 const box = document.querySelector('.div--js');
 
 box.innerHTML = 'Treść podstawiona przez JS';
+
+
+function calculate(myNumber){
+    return myNumber*7
+};
+
+const myResoult = calculate(2);
+
+console.log(myResoult); 
+
+function greet(age, irstName){ /*nazwy argumentów funkcji muszą być takie same jak zmienne w ciele finkcji inaczej, zmienne z ciała funkcji będą "szukać takich samych nazw w kodzie"*/
+    console.log(
+        `Witaj Drogi Odwiedzający, nazywam się ${irstName} i mam ${age} lata`
+    );
+}
+
+greet(34, 'Janek');
+
+greet('Dawid', 18);
+/*wartości argumentów przekazywane są do "nazw" argumentów funkcji i później wykożystywane w ciele funkcji. Kolejność wartości argumentów mają znaczenie.
+Wpisanie greet('Dawid', 18) spowoduje podstawienie 18 do zmiennej irstName  a Dawid do zmiennej age*/
